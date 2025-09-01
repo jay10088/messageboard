e4e9ee4b464bc103a4a5e67277d42d78b689dcdd
jay <jay@jaydeMacBook-Air.local>
2025-09-01 11:32:45 +0800

    feat(login): 新增角色權限判斷及處理 feat(point): 新增點數管理介面及相關api feat(config): 新增外網連線設定 fix(comment): 部分檔案補充comment feat(staff): 新增staff刪除留言權限 chore(migration): 修改migration style(model): model大小寫修正

--- app/controller/point.js
+++ app/controller/point.js
@@ -3,25 +3,127 @@
 const Controller = require('egg').Controller;
 
 class PointController extends Controller {
+
+  //指定使用者的點數資訊
+  async pointInfo() {
+    const { ctx } = this;
+
+    //驗證
+    const rules = {
+      username: { type: 'string', required: true, allowEmpty: false },
+    }
+    ctx.validate(rules , ctx.params);
+
+    const { username } = ctx.params;
+  
+    const rows = await ctx.model.User.findOne({
+      where: { username },
+      attributes: ['username', 'point'],
+      raw: true,
+    });
+
+    ctx.body = rows;
+  }
+
+  //顯示全部點數交易歷史
+  async showAllPointHistory() {
+    const { ctx } = this;
+  
+    const rows = await ctx.model.Point.findAll({
+      attributes: ['id', 'username', 'delta', 'createdAt'],
+      order: [['id', 'DESC']],
+      limit: 18,
+      raw: true,
+    });
+
+    ctx.body = rows;
+  }
+
+  //顯示特定使用者交易歷史
+  async showUserPointHistory() {
+    const { ctx } = this;
+    const { username } = ctx.params;
+
+    //驗證
+    const rules = {
+      username: { type: 'string', required: true, allowEmpty: false },
+    }
+    ctx.validate(rules , ctx.params);
+  
+    const rows = await ctx.model.Point.findAll({
+      where: { username },
+      attributes: ['id', 'username', 'delta', 'createdAt'],
+      order: [['id', 'DESC']],
+      limit: 18,
+      raw: true,
+    });
+
+    ctx.body = rows;
+  }
+
+  //儲值目前使用者點數(給一般使用者使用)
   async addPoint() {
     const { ctx } = this;
-    const returnStatus = 200;
-    const returnBody = { msg: '儲值成功' };
+    let returnStatus = 200;
+    let returnBody = { msg: '儲值成功' };
+
+    //驗證
+    const rule = {
+      point: { type: 'int', min: 1, required: true, convertType: 'int' }
+    };
+    ctx.validate(rule, ctx.request.body);
+
+    const { point } = ctx.request.body;
+    const username = ctx.session.user.username;
+
+    //原子操作資料庫
+    const t = await ctx.model.transaction();
+    try {
+      await ctx.model.User.increment('point', { by: point, where: { username: username }, transaction: t } );
+      await ctx.model.Point.create( { delta: point, username: username }, { transaction: t } );
+      await t.commit();
+    } catch (err) {
+      await t.rollback();
+      returnStatus = 400;
+      returnBody = { msg: '寫入資料庫失敗' };
+    }
+      
+    ctx.status = returnStatus;
+
+    ctx.body = returnBody;
+  }
+
+  //管理特定使用者點數（只開放給管理員用）
+  async managePoint() {
+    const { ctx } = this;
+    let returnStatus = 200;
+    let returnBody = { msg: '儲值成功' };
 
     //驗證
     const pointRule = {
-      id: { type: 'int', min: 1, required: true, convertType: 'int' }
+      point: { type: 'int', required: true, convertType: 'int' }
     };
-    ctx.validate(rules.pointRule, ctx.request.body);
+    ctx.validate(pointRule, ctx.request.body);
+    const paramsRule = {
+      username: { type: 'string', required: true, allowEmpty: false },
+    }
+    ctx.validate(paramsRule , ctx.params);
 
     const { point } = ctx.request.body;
-    const userId = ctx.session.user.id;
-    
-    await ctx.model.User.increment('point', {
-      by: point,
-      where: { id: userId },
-    });
+    const { username } = ctx.params;
 
+    //原子操作資料庫
+    const t = await ctx.model.transaction();
+    try {
+      await ctx.model.User.increment('point', { by: point, where: { username: username }, transaction: t } );
+      await ctx.model.Point.create( { delta: point, username: username }, { transaction: t } );
+      await t.commit();
+    } catch (err) {
+      await t.rollback();
+      returnStatus = 400;
+      returnBody = { msg: '寫入資料庫失敗' };
+    }
+       
     ctx.status = returnStatus;
 
     ctx.body = returnBody;
--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,10 +1,40 @@
-module.exports = () => {
+module.exports = (requiredRoles = []) => {
+  //數字越高，權限越高
+  const roleHierarchy = {
+    user: 1,
+    staff: 2,
+    admin: 3
+  };
+
   return async function auth(ctx, next) {
+    let returnStatus = 400;
+    let returnBody = { msg: '未登入' };
+    let shouldResponse = true;
+
+    // 檢查是否已登入
     if (ctx.session.user) {
-      await next();
-    } else {
-      ctx.status = 401;
-      ctx.body = { msg: '未登入' };
+      const user = ctx.session.user;
+      const userRole = user.role;
+
+      // 檢查使用者權限
+      const userLevel = roleHierarchy[userRole];
+      const hasPermission = requiredRoles.some(role => userLevel >= roleHierarchy[role]);
+      
+      //有權限才放行
+      if (hasPermission) {
+        shouldResponse = false; // 有權限時不回傳
+        await next();
+      } else {
+        returnStatus = 400;
+        returnBody = { msg: '權限不足' };
+      }
+    }
+
+    // 只有沒權限or未登入才回傳
+    if (shouldResponse) {
+      ctx.status = returnStatus;
+      
+      ctx.body = returnBody;
     }
   };
 };
